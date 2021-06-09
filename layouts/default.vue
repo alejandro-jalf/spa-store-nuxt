aad835be06280b4daf5f1c773ffab20e7828a8b8
Jose Alejandro Lopez Flores <aleflo_1996@outlook.com>
2021-06-09 15:38:13 -0500

    FIX: Disminuye tamaño de deslizamiento

--- layouts/default.vue
+++ layouts/default.vue
@@ -256,13 +256,13 @@ export default {
 
         if (this.moveTouch === 'right') {
           newPosition = positionActual - 1
-          if (newPosition >= 0 && diffAbsolute > 120 && tabs[newPosition])
+          if (newPosition >= 0 && diffAbsolute > 50 && tabs[newPosition])
             this.$router.replace({ path: tabs[newPosition].path })
         } else {
           newPosition = positionActual + 1
           if (
             newPosition <= countTabs &&
-            diffAbsolute > 120 &&
+            diffAbsolute > 50 &&
             tabs[newPosition]
           )
             this.$router.replace({ path: tabs[newPosition].path })
