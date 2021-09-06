f0715ab3352bbca0b673db7f2d55d338252f7001
Jose Alejandro Lopez Flores <aleflo_1996@outlook.com>
2021-09-06 13:19:09 -0500

    REFACTOR: Mejora responsividad en crecimiento cocina

--- components/CocinaCrecimiento.vue
+++ components/CocinaCrecimiento.vue
@@ -23,6 +23,7 @@
       {{ tendencia }}
     </div>
     <b-table
+      v-if="width > 991"
       striped
       hover
       head-variant="dark"
@@ -33,6 +34,48 @@
       class="mt-2"
       :tbody-tr-class="rowClass"
     ></b-table>
+    <div v-else class="mt-2">
+      <b-card
+        v-for="(dia, indexDia) in itemsRafactor"
+        :key="indexDia"
+        no-body
+        class="containerCard"
+        :class="variantTheme"
+      >
+        <div class="dayTotal">
+          <span v-if="dia.status !== 'end'">Dia: </span>{{ dia.Dia }}
+        </div>
+        <div :class="isNow(dia)">
+          <div>
+            <span class="font-weight-bold">{{ fields[1] }}: </span>
+            {{ dia[`${fields[1]}`] }},
+          </div>
+          <div>
+            <span class="font-weight-bold">{{ fields[2] }}: </span>
+            {{ dia[`${fields[2]}`] }}
+          </div>
+          <div :class="dia._cellVariants.CrecimientoDiario">
+            <span class="font-weight-bold">Crecimiento Diario: </span>
+            {{ dia.CrecimientoDiario }}
+          </div>
+          <div v-if="dia.status !== 'end'">
+            <span class="font-weight-bold">Acumulado {{ fields[1] }}: </span>
+            {{ dia[`Acumulado${fields[1]}`] }}
+          </div>
+          <div v-if="dia.status !== 'end'">
+            <span class="font-weight-bold">Acumulado {{ fields[2] }}: </span>
+            {{ dia[`Acumulado${fields[2]}`] }}
+          </div>
+          <div
+            v-if="dia.status !== 'end'"
+            :class="dia._cellVariants.CrecimientoAcumulado"
+          >
+            <span class="font-weight-bold">Crecimiento Acumulado: </span>
+            {{ dia.CrecimientoAcumulado }}
+          </div>
+        </div>
+      </b-card>
+    </div>
     <h2>Grafico</h2>
     <div class="mb-5">
       <b-button variant="info" @click="changeGrafico('bar')">
@@ -82,6 +125,21 @@ export default {
     }
   },
   computed: {
+    width() {
+      return this.$store.state.general.widthWindow
+    },
+    variantTheme() {
+      if (this.$store.state.general.themePreferences === 'system') {
+        const systemDark = window.matchMedia('(prefers-color-scheme: dark)')
+          .matches
+        if (systemDark) return 'darkBodyCard containerCarDark'
+        return 'containerCard'
+      } else if (this.$store.state.general.themePreferences === 'dark')
+        return 'darkBodyCard containerCarDark'
+      else if (this.$store.state.general.themePreferences === 'sepia')
+        return 'sepiaBodyCard containerCard'
+      else return 'containerCard'
+    },
     showGraph() {
       return this.$store.state.cocina.showGraph
     },
@@ -117,6 +175,8 @@ export default {
         if (diaFinded === -1) {
           const newSuc = {
             Dia: sucursal.Dia,
+            Mes: [sucursal.Mes],
+            Year: sucursal.Year,
             CrecimientoDiario: '-100.00 %',
             _cellVariants: { CrecimientoDiario: 'danger' },
           }
@@ -223,6 +283,18 @@ export default {
       setTipoGrafico: 'cocina/setTipoGrafico',
       setShowGraph: 'cocina/setShowGraph',
     }),
+    isNow(diaVenta) {
+      const now = new Date()
+      if (diaVenta.Mes) {
+        const month = diaVenta.Mes.find((mes) => now.getMonth() + 1)
+        return diaVenta.Dia === now.getDate() &&
+          diaVenta.Year === now.getFullYear() &&
+          month
+          ? 'centerCard'
+          : ''
+      }
+      return ''
+    },
     rowClass(item, type) {
       if (!item || type !== 'row') return
       if (item.status === 'end') return 'table-primary'
@@ -246,6 +318,47 @@ export default {
 </script>
 
 <style scoped>
+.danger {
+  padding: 5px 5px 5px 2px;
+  border-radius: 3px;
+  background: rgb(255, 172, 172);
+}
+
+.success {
+  padding: 5px 5px 5px 2px;
+  border-radius: 3px;
+  background: rgb(172, 255, 172);
+}
+
+.dayTotal {
+  font-weight: bold;
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  background: #018299;
+  color: #fff;
+  border-radius: 3px 0px 5px 0px;
+  width: 58px;
+  text-align: center;
+}
+
+.containerCard {
+  padding: 27px 15px;
+  margin-bottom: 10px;
+  box-shadow: 2px 2px 2px #e6e6e6;
+}
+
+.containerCarDark {
+  box-shadow: 2px 2px 2px #5d5d5d;
+}
+
+.centerCard {
+  padding: 10px;
+  border-radius: 5px;
+  border: 1px solid rgb(226, 226, 226);
+  background: rgba(11, 255, 3, 0.472);
+}
+
 .container-result {
   display: inline-block;
   margin-top: 10px;
