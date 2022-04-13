549ca3797f1e01acb1ac74efba116191e8f52396
alejandro-jalf <aleflo_1996@outlook.com>
2022-04-13 09:24:49 -0500

    REFACTOR: Cambia liga de consultas de conexiones

--- store/conexiones.js
+++ store/conexiones.js
@@ -19,10 +19,7 @@ export const actions = {
       const sucSplited = sucursal.split(' ')
       const company =
         sucSplited[0].trim().toUpperCase() === 'CAASA' ? 'caasa' : 'spa'
-      const url =
-        company === 'spa'
-          ? process.env.spastore_url_conexiones
-          : `${urlBase}api/v1/general/${company}/conexiones/activas`
+      const url = `${urlBase}api/v1/general/${company}/conexiones/activas`
       const response = await this.$axios({
         url,
         method: 'get',
