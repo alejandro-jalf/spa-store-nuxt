4d33757b24a3a3959448c8f854b0de2980b34a6e
alejandro-jalf <aleflo_1996@outlook.com>
2022-05-10 18:03:34 -0500

    ADD: Agrega store para ventas diarias

---
+++ store/ventasdiarias.js
@@ -0,0 +1,86 @@
+if (!localStorage.getItem('spastore_ventasdiarias_sucursal'))
+  localStorage.setItem('spastore_ventasdiarias_sucursal', null)
+if (!localStorage.getItem('spastore_ventasdiarias_dateInit'))
+  localStorage.setItem('spastore_ventasdiarias_dateInit', '00/00/0000')
+if (!localStorage.getItem('spastore_ventasdiarias_dateEnd'))
+  localStorage.setItem('spastore_ventasdiarias_dateEnd', '00/00/0000')
+if (!localStorage.getItem('spastore_ventasdiarias_tipo'))
+  localStorage.setItem('spastore_ventasdiarias_tipo', 'line')
+
+export const state = () => ({
+  data: localStorage.getItem('spastore_ventasdiarias_data')
+    ? JSON.parse(localStorage.getItem('spastore_ventasdiarias_data'))
+    : { data: [] },
+  sucursal: localStorage.getItem('spastore_ventasdiarias_sucursal'),
+  dateInit: localStorage.getItem('spastore_ventasdiarias_dateInit'),
+  dateEnd: localStorage.getItem('spastore_ventasdiarias_dateEnd'),
+  tipo: localStorage.getItem('spastore_ventasdiarias_tipo'),
+  showGraph: true,
+})
+
+export const mutations = {
+  setData(state, data) {
+    state.data = data
+    localStorage.setItem('spastore_ventasdiarias_data', JSON.stringify(data))
+  },
+  cleanData(state) {
+    state.data = { data: [] }
+    localStorage.removeItem('spastore_ventasdiarias_data')
+  },
+  setSucursal(state, sucursal) {
+    state.sucursal = sucursal
+    localStorage.setItem('spastore_ventasdiarias_sucursal', sucursal)
+  },
+  setDateInit(state, dateInit) {
+    state.dateInit = dateInit
+    localStorage.setItem('spastore_ventasdiarias_dateInit', dateInit)
+  },
+  setDateEnd(state, dateEnd) {
+    state.dateEnd = dateEnd
+    localStorage.setItem('spastore_ventasdiarias_dateEnd', dateEnd)
+  },
+  setTipoGrafico(state, tipo) {
+    state.tipo = tipo
+    localStorage.setItem('spastore_ventasdiarias_tipo', tipo)
+  },
+  setShowGraph(state, showGraph) {
+    state.showGraph = showGraph
+  },
+}
+
+export const actions = {
+  async changeData(
+    { commit },
+    [sucursal, dateStart, dateEnd, dateStartLetra, dateEndLetra]
+  ) {
+    try {
+      const url =
+        process.env.spastore_url_backend +
+        'api/v1/reportes/ventas/' +
+        sucursal +
+        '?FechaIni=' +
+        dateStart +
+        '&FechaFin=' +
+        dateEnd
+      const response = await this.$axios({
+        url,
+        method: 'get',
+      })
+
+      if (response.data.success) {
+        commit('setData', { data: response.data.data })
+        commit('setDateInit', dateStartLetra)
+        commit('setDateEnd', dateEndLetra)
+      }
+
+      return response.data
+    } catch (error) {
+      if (error.response) return error.response.data
+      return {
+        success: false,
+        message: 'Error con el servidor',
+        error,
+      }
+    }
+  },
+}
