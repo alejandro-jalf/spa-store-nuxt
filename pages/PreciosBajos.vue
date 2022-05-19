482911e9b2315bd8b30a199ae302496c9b68e1d1
alejandro-jalf <aleflo_1996@outlook.com>
2022-05-18 19:53:06 -0500

    ADD: Agrega store para precios bajos

--- pages/PreciosBajos.vue
+++ pages/PreciosBajos.vue
@@ -1,3 +1,65 @@
 <template>
-  <div></div>
+  <div>
+    <h1 class="text-center mt-2">Articulos con precios Bajos</h1>
+    <b-input-group prepend="Sucursal" class="mt-3 mb-3">
+      <b-form-select
+        :value="suc"
+        :options="options"
+        @change="selectSucursal"
+      ></b-form-select>
+      <b-input-group-append>
+        <b-form-input
+          id="input-porcentaje"
+          v-model="porcentaje"
+          v-b-tooltip.hover
+          title="% Minimo"
+          value="10"
+          placeholder="% Min."
+          type="number"
+          size="sm"
+          class="h-100 w-25"
+          :class="backgroundInputTheme"
+        ></b-form-input>
+        <b-button variant="info">Verificar</b-button>
+      </b-input-group-append>
+    </b-input-group>
+  </div>
 </template>
+
+<script>
+import { mapMutations } from 'vuex'
+
+export default {
+  data() {
+    return {
+      options: [
+        { value: 'ZR', text: 'SPAZARAGOZA' },
+        { value: 'VC', text: 'SPAVICTORIA' },
+        { value: 'ER', text: 'SPAENRIQUEZ' },
+        { value: 'OU', text: 'SPAOLUTA' },
+        { value: 'SY', text: 'SPASAYULA' },
+        { value: 'JL', text: 'SPAJALTIPAN' },
+        { value: 'BO', text: 'SPABODEGA' },
+      ],
+      porcentaje: 10,
+    }
+  },
+  computed: {
+    suc() {
+      return this.$store.state.preciosbajos.sucursal
+    },
+    backgroundInputTheme() {
+      return this.$store.state.general.themesComponents.themeInputBackground
+    },
+  },
+  methods: {
+    ...mapMutations({
+      setSucursal: 'preciosbajos/setSucursal',
+    }),
+    selectSucursal(sucursal) {
+      console.log(sucursal)
+      this.setSucursal(sucursal)
+    },
+  },
+}
+</script>
---
+++ store/preciosbajos.js
@@ -0,0 +1,52 @@
+if (!localStorage.getItem('spastore_preciosbajos_sucursal'))
+  localStorage.setItem('spastore_preciosbajos_sucursal', 'ZR')
+
+export const state = () => ({
+  sucursal: localStorage.getItem('spastore_preciosbajos_sucursal'),
+  data: localStorage.getItem('spastore_preciosbajos_data')
+    ? JSON.parse(localStorage.getItem('spastore_preciosbajos_data'))
+    : { data: [] },
+})
+
+export const mutations = {
+  setSucursal(state, sucursal) {
+    state.sucursal = sucursal
+    localStorage.setItem('spastore_preciosbajos_sucursal', sucursal)
+  },
+  setData(state, data) {
+    state.data = data
+    localStorage.setItem('spastore_preciosbajos_data', JSON.stringify(data))
+  },
+}
+
+export const actions = {
+  async changeData({ commit }, sucursal, procentaje) {
+    try {
+      const url =
+        process.env.spastore_url_backend +
+        'api/v1/articulos/utilidades/' +
+        sucursal +
+        '?porcentajeUtilidad=' +
+        procentaje
+      const response = await this.$axios({
+        url,
+        method: 'get',
+      })
+
+      if (response.data.success) {
+        commit('setData', { data: response.data.data })
+      }
+
+      return response.data
+    } catch (error) {
+      if (error.response) {
+        return error.response.data
+      }
+      return {
+        success: false,
+        message: 'Error con el servidor',
+        error,
+      }
+    }
+  },
+}
