59b6476e9b2919dbf61e1e283c38eae4d5479505
alejandro-jalf <aleflo_1996@outlook.com>
2022-05-24 19:23:23 -0500

    REFACTOR: Agrega configuracion para cambio de idioma

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,18 +2,13 @@ export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     title: 'spa-store',
+    htmlAttrs: { lang: 'es' },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/cesta.png' }],
-  },
-
-  pwa: {
-    manifest: {
-      lang: 'es',
-    },
   },
 
   router: {
