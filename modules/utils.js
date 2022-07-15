7652af10c26ace4645c16b1ca549382e0a1f23a8
alejandro-jalf <aleflo_1996@outlook.com>
2022-07-15 14:14:26 -0500

    ADD: Agrega libreria para pasar numeros a letras

--- modules/utils.js
+++ modules/utils.js
@@ -18,6 +18,207 @@ const utils = (() => {
     'Diciembre',
   ]
 
+  const numeroALetras = (() => {
+    const Unidades = (num) => {
+      switch (num) {
+        case 1:
+          return 'UN'
+        case 2:
+          return 'DOS'
+        case 3:
+          return 'TRES'
+        case 4:
+          return 'CUATRO'
+        case 5:
+          return 'CINCO'
+        case 6:
+          return 'SEIS'
+        case 7:
+          return 'SIETE'
+        case 8:
+          return 'OCHO'
+        case 9:
+          return 'NUEVE'
+      }
+
+      return ''
+    } // Unidades()
+
+    const Decenas = (num) => {
+      const decena = Math.floor(num / 10)
+      const unidad = num - decena * 10
+
+      switch (decena) {
+        case 1:
+          switch (unidad) {
+            case 0:
+              return 'DIEZ'
+            case 1:
+              return 'ONCE'
+            case 2:
+              return 'DOCE'
+            case 3:
+              return 'TRECE'
+            case 4:
+              return 'CATORCE'
+            case 5:
+              return 'QUINCE'
+            default:
+              return 'DIECI' + Unidades(unidad)
+          }
+        case 2:
+          switch (unidad) {
+            case 0:
+              return 'VEINTE'
+            default:
+              return 'VEINTI' + Unidades(unidad)
+          }
+        case 3:
+          return DecenasY('TREINTA', unidad)
+        case 4:
+          return DecenasY('CUARENTA', unidad)
+        case 5:
+          return DecenasY('CINCUENTA', unidad)
+        case 6:
+          return DecenasY('SESENTA', unidad)
+        case 7:
+          return DecenasY('SETENTA', unidad)
+        case 8:
+          return DecenasY('OCHENTA', unidad)
+        case 9:
+          return DecenasY('NOVENTA', unidad)
+        case 0:
+          return Unidades(unidad)
+      }
+    } // Unidades()
+
+    const DecenasY = (strSin, numUnidades) => {
+      if (numUnidades > 0) return strSin + ' Y ' + Unidades(numUnidades)
+      return strSin
+    } // DecenasY()
+
+    const Centenas = (num) => {
+      const centenas = Math.floor(num / 100)
+      const decenas = num - centenas * 100
+
+      switch (centenas) {
+        case 1:
+          if (decenas > 0) return 'CIENTO ' + Decenas(decenas)
+          return 'CIEN'
+        case 2:
+          return 'DOSCIENTOS ' + Decenas(decenas)
+        case 3:
+          return 'TRESCIENTOS ' + Decenas(decenas)
+        case 4:
+          return 'CUATROCIENTOS ' + Decenas(decenas)
+        case 5:
+          return 'QUINIENTOS ' + Decenas(decenas)
+        case 6:
+          return 'SEISCIENTOS ' + Decenas(decenas)
+        case 7:
+          return 'SETECIENTOS ' + Decenas(decenas)
+        case 8:
+          return 'OCHOCIENTOS ' + Decenas(decenas)
+        case 9:
+          return 'NOVECIENTOS ' + Decenas(decenas)
+      }
+
+      return Decenas(decenas)
+    } // Centenas()
+
+    const Seccion = (num, divisor, strSingular, strPlural) => {
+      const cientos = Math.floor(num / divisor)
+      const resto = num - cientos * divisor
+
+      let letras = ''
+
+      if (cientos > 0)
+        if (cientos > 1) letras = Centenas(cientos) + ' ' + strPlural
+        else letras = strSingular
+
+      if (resto > 0) letras += ''
+
+      return letras
+    } // Seccion()
+
+    const Miles = (num) => {
+      const divisor = 1000
+      const cientos = Math.floor(num / divisor)
+      const resto = num - cientos * divisor
+
+      const strMiles = Seccion(num, divisor, 'UN MIL', 'MIL')
+      const strCentenas = Centenas(resto)
+
+      if (strMiles === '') return strCentenas
+
+      return strMiles + ' ' + strCentenas
+    } // Miles()
+
+    function Millones(num) {
+      const divisor = 1000000
+      const cientos = Math.floor(num / divisor)
+      const resto = num - cientos * divisor
+
+      const strMillones = Seccion(num, divisor, 'UN MILLON DE', 'MILLONES DE')
+      const strMiles = Miles(resto)
+
+      if (strMillones === '') return strMiles
+
+      return strMillones + ' ' + strMiles
+    } // Millones()
+
+    return (num = 0.0, currency = {}, centavos = true) => {
+      const data = {
+        numero: num,
+        enteros: Math.floor(num),
+        centavos: Math.round(num * 100) - Math.floor(num) * 100,
+        letrasCentavos: '',
+        letrasMonedaPlural: currency.plural || 'PESOS', // 'PESOS', 'Dólares', 'Bolívares', 'etcs'
+        letrasMonedaSingular: currency.singular || 'PESO', // 'PESO', 'Dólar', 'Bolivar', 'etc'
+        letrasMonedaCentavoPlural: currency.centPlural || 'CENTAVOS',
+        letrasMonedaCentavoSingular: currency.centSingular || 'CENTAVO',
+      }
+
+      if (!centavos) {
+        if (data.centavos < 10)
+          data.letrasCentavos = '0' + data.centavos + '/100 MN'
+        else data.letrasCentavos = data.centavos + '/100 MN'
+      } else if (data.centavos > 0) {
+        data.letrasCentavos =
+          'CON ' +
+          (() => {
+            if (data.cen === 1)
+              return (
+                Millones(data.centavos) + ' ' + data.letrasMonedaCentavoSingular
+              )
+            else
+              return (
+                Millones(data.centavos) + ' ' + data.letrasMonedaCentavoPlural
+              )
+          })()
+      }
+
+      if (data.enteros === 0)
+        return 'CERO ' + data.letrasMonedaPlural + ' ' + data.letrasCentavos
+      if (data.enteros === 1)
+        return (
+          Millones(data.enteros) +
+          ' ' +
+          data.letrasMonedaSingular +
+          ' ' +
+          data.letrasCentavos
+        )
+      else
+        return (
+          Millones(data.enteros) +
+          ' ' +
+          data.letrasMonedaPlural +
+          ' ' +
+          data.letrasCentavos
+        )
+    }
+  })()
+
   const completeDateHour = (number) => {
     if (!number) return number
     const numberString = number.toString()
@@ -716,6 +917,7 @@ const utils = (() => {
     createPdfPedidoSujerido,
     createPdfAsistenciasSpa,
     createPdfValuacionInventarioCierre,
+    numeroALetras,
   }
 })()
 
