2ca6dc75b969eb3760e1aaa4348db3b7bce0d056
alejandro-jalf <aleflo_1996@outlook.com>
2023-04-13 15:53:49 -0600

    REFACTOR: Mejora bases de datos al recordar la sucursal en donde se encuentra

--- pages/DataBases.vue
+++ pages/DataBases.vue
@@ -2,31 +2,59 @@
   <div>
     <h3 class="text-center py-3">Bases de datos</h3>
     <b-tabs content-class="mt-3">
-      <b-tab title="Zaragoza" active>
+      <b-tab
+        title="Zaragoza"
+        :active="isActiveSucursal('ZR')"
+        @click="setSucursal('ZR')"
+      >
         <DatabBasesSucursal sucursal="ZR" />
         <DatabBasesSucursalChart v-if="showGraph" sucursal="ZR" />
       </b-tab>
-      <b-tab title="Victoria">
+      <b-tab
+        title="Victoria"
+        :active="isActiveSucursal('VC')"
+        @click="setSucursal('VC')"
+      >
         <DatabBasesSucursal sucursal="VC" />
         <DatabBasesSucursalChart v-if="showGraph" sucursal="VC" />
       </b-tab>
-      <b-tab title="Enriquez">
+      <b-tab
+        title="Enriquez"
+        :active="isActiveSucursal('ER')"
+        @click="setSucursal('ER')"
+      >
         <DatabBasesSucursal sucursal="ER" />
         <DatabBasesSucursalChart v-if="showGraph" sucursal="ER" />
       </b-tab>
-      <b-tab title="Oluta">
+      <b-tab
+        title="Oluta"
+        :active="isActiveSucursal('OU')"
+        @click="setSucursal('OU')"
+      >
         <DatabBasesSucursal sucursal="OU" />
         <DatabBasesSucursalChart v-if="showGraph" sucursal="OU" />
       </b-tab>
-      <b-tab title="Sayula">
+      <b-tab
+        title="Sayula"
+        :active="isActiveSucursal('SY')"
+        @click="setSucursal('SY')"
+      >
         <DatabBasesSucursal sucursal="SY" />
         <DatabBasesSucursalChart v-if="showGraph" sucursal="SY" />
       </b-tab>
-      <b-tab title="Jaltipan">
+      <b-tab
+        title="Jaltipan"
+        :active="isActiveSucursal('JL')"
+        @click="setSucursal('JL')"
+      >
         <DatabBasesSucursal sucursal="JL" />
         <DatabBasesSucursalChart v-if="showGraph" sucursal="JL" />
       </b-tab>
-      <b-tab title="Bodega">
+      <b-tab
+        title="Bodega"
+        :active="isActiveSucursal('BO')"
+        @click="setSucursal('BO')"
+      >
         <DatabBasesSucursal sucursal="BO" />
         <DatabBasesSucursalChart v-if="showGraph" sucursal="BO" />
       </b-tab>
@@ -58,6 +86,7 @@ export default {
   methods: {
     ...mapMutations({
       setShowGraph: 'databases/setShowGraph',
+      setSucursal: 'databases/setSucursal',
     }),
     refreshGrafico() {
       const that = this
@@ -65,6 +94,10 @@ export default {
       setTimeout(() => {
         that.setShowGraph(true)
       }, 100)
+    },
+    isActiveSucursal(sucursal) {
+      const sucursalF = this.$store.state.databases.sucursal
+      return sucursal.toUpperCase() === sucursalF.toUpperCase()
     },
   },
 }
